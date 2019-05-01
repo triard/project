a0ba0996635420a33aa988816cf35c070b1fcd1a
triard <triard78@gmail.com>
2019-05-01 20:11:00 +0700

    membuat kotak tabel lebih menarik

--- config/koneksi (2).php
+++
@@ -1,9 +0,0 @@
-<?php 
-$db = mysqli_connect("localhost","root","","lets_go");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
- 
-?>
